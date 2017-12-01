c6b60a39febe133d95d108d13e5dfafd2fda0cbf
albracu <yo@alfredobravocuero.co>
2017-12-01 18:18:31 -0500

    Definicion Basica de Matrices

---
+++ src/matrices/Matrices.java
@@ -0,0 +1,24 @@
+package matrices;
+
+public class Matrices {
+
+    public static void main(String[] args) {
+        String nombre[] = new String[10];
+        String ciudad[] = new String[10];
+
+        nombre[0] = "Juan";
+        nombre[1] = "Javier";
+        nombre[2] = "Jaime";
+        nombre[3] = "Jairo";
+        nombre[4] = "Julio";
+
+        ciudad[0] = "Medellin";
+        ciudad[1] = "Cali";
+        ciudad[2] = "Bogota";
+
+        System.out.println(nombre[3]);
+        System.out.println(ciudad[2]);
+
+    }
+
+}
