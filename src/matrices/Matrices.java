b754d3b2f38098d2c9bfc83c6cf8bb2ccce6c03c
albracu <yo@alfredobravocuero.co>
2017-12-01 18:53:19 -0500

    Rama Matriz Multidimenciona.

--- src/matrices/Matrices.java
+++ src/matrices/Matrices.java
@@ -3,22 +3,10 @@ package matrices;
 public class Matrices {
 
     public static void main(String[] args) {
-        String nombre[] = {"Alfredo", "Milena", "Isabella", "Nelly", "Andrea"};
-        //String nombre[] = new String[10];
-        String ciudad[] = new String[10];
+        String nombres[][] = {{"Alfredo", "Milena", "Isabella", "Nelly", "Andrea"},{"Diego Andres", "Neira", "Bolivar", "Jhonatan", "Gabriel"}};
 
-        /*nombre[0] = "Juan";
-        nombre[1] = "Javier";
-        nombre[2] = "Jaime";
-        nombre[3] = "Jairo";
-        nombre[4] = "Julio";*/
+        System.out.println(nombres[0][0]);
 
-        ciudad[0] = "Medellin";
-        ciudad[1] = "Cali";
-        ciudad[2] = "Bogota";
-
-        System.out.println(nombre[3]);
-        System.out.println(ciudad[2]);
 
     }
 
