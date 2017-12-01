4c4287a690e18ad790123deec9b6899a2c6f147d
albracu <yo@alfredobravocuero.co>
2017-12-01 18:47:02 -0500

    Definicion Matrices eficientes.

--- src/matrices/Matrices.java
+++ src/matrices/Matrices.java
@@ -3,14 +3,15 @@ package matrices;
 public class Matrices {
 
     public static void main(String[] args) {
-        String nombre[] = new String[10];
+        String nombre[] = {"Alfredo", "Milena", "Isabella", "Nelly", "Andrea"};
+        //String nombre[] = new String[10];
         String ciudad[] = new String[10];
 
-        nombre[0] = "Juan";
+        /*nombre[0] = "Juan";
         nombre[1] = "Javier";
         nombre[2] = "Jaime";
         nombre[3] = "Jairo";
-        nombre[4] = "Julio";
+        nombre[4] = "Julio";*/
 
         ciudad[0] = "Medellin";
         ciudad[1] = "Cali";
